450967d8e83385c896608dc83d8cd9e99bf91722
Maxime Bever <36727317+Maxbever@users.noreply.github.com>
2022-08-04 18:18:00 +0200

    Update traffic light

--- src/traffic_light.rs
+++ src/traffic_light.rs
@@ -1,5 +1,3 @@
-use std::{thread};
-use std::time::Duration;
 use rand::Rng;
 use rustupolis_server::client::Client;
 use rustupolis_server::repository::Repository;
@@ -7,6 +5,9 @@ use rustupolis_server::server::{Protocol, Server};
 use rustupolis_server::server_launcher::ServerLauncher;
 use rustupolis_server::tuple;
 use rustupolis_server::tuple::E;
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc::channel;
 
 pub fn traffic_light(traffic_id: i32) {
     let ip_address = String::from("127.0.0.1");
@@ -16,12 +17,20 @@ pub fn traffic_light(traffic_id: i32) {
     let mut client = Client::new();
     let mut state = 0;
 
-    repository.add_tuple_space(String::from("Traffic_light_") + &*traffic_id.to_string(), vec![String::from("admin")]);
-    repository.add_tuple_to_tuple_space(String::from("Traffic_light_") + &*traffic_id.to_string(), tuple![E::str("state"), E::str("green")]);
+    repository.add_tuple_space(
+        String::from("Traffic_light_") + &*traffic_id.to_string(),
+        vec![String::from("admin")],
+    );
+    repository.add_tuple_to_tuple_space(
+        String::from("Traffic_light_") + &*traffic_id.to_string(),
+        tuple![E::str("state"), E::str("red")],
+    );
 
-    let server_tcp = Server::new(Protocol::TCP, &ip_address, &port_tcp, &repository,key);
+    let server_tcp = Server::new(Protocol::TCP, &ip_address, &port_tcp, &repository, key);
 
     let server_launcher = ServerLauncher::new(vec![server_tcp]);
+
+    let (sender, receiver) = channel();
 
     crossbeam::scope(|scope| {
         scope.spawn(move |_| {
@@ -38,13 +47,11 @@ pub fn traffic_light(traffic_id: i32) {
             let mut port_first_layer = String::from("9015");
             if traffic_id < 4 {
                 port_first_layer = String::from("9013");
-            }else {
+            } else {
                 if traffic_id < 8 {
                     port_first_layer = String::from("9014");
                 }
             }
-
-            thread::sleep(Duration::new(30, 0));
 
             client.connect(
                 ip_address,
@@ -57,44 +64,64 @@ pub fn traffic_light(traffic_id: i32) {
             client.attach(&first_layer, vec![attribute.clone()], &tuple_space_name);
 
             loop {
-                thread::sleep(Duration::new(30, 0));
                 let random_number = rand::thread_rng().gen_range(5..15);
 
-                client.out(vec![tuple![E::str("nbr_cars_passing"), E::I(random_number)]]);
+                client.out(vec![tuple![
+                    E::str("nbr_cars_passing"),
+                    E::I(random_number)
+                ]]);
 
-                if state == 1  {
+                let response = receiver.recv().unwrap();
+                dbg!(response);
+
+                if response == 1 {
                     let random_number = rand::thread_rng().gen_range(4..8);
 
-                    client.out(vec![tuple![E::str("nbr_cars_waiting"), E::I(random_number)]])
+                    client.out(vec![tuple![
+                        E::str("nbr_cars_waiting"),
+                        E::I(random_number)
+                    ]])
                 }
+                thread::sleep(Duration::new(15, 0));
             }
         });
 
+        client.connect(
+            ip_address.clone(),
+            port_tcp.clone(),
+            String::from("tcp"),
+            &String::from("Traffic_light"),
+            key,
+        );
+
+        client.attach(
+            &String::from("Traffic_light"),
+            vec![String::from("admin")],
+            &(String::from("Traffic_light_") + &*traffic_id.to_string()),
+        );
+
         loop {
-            client.connect(
-                ip_address.clone(),
-                port_tcp.clone(),
-                String::from("tcp"),
-                &String::from("Traffic_light"),
-                key,
-            );
 
-            client.attach(&String::from("Traffic_light"), vec![String::from("admin")], &(String::from("Traffic_light_") + &*traffic_id.to_string()));
+            let state_tuple = client.read(vec![tuple![E::str("state"), E::Any]]);
 
-            let state_tuple = client.in_instr(vec![tuple![E::str("state"),E::Any]]);
+            if !state_tuple.is_empty() {
+                state = match state_tuple.rest().first() {
+                    E::S(data) => {
+                        if data == "green" {
+                            0
+                        } else {
+                            1
+                        }
+                    }
+                    _ => {
+                        panic!("Error");
+                    }
+                };
+                sender.send(state).unwrap();
+            }
 
-            state = match state_tuple.rest().first() {
-                E::I(data) => {
-                    data.clone()
-                }
-                _ => {
-                    panic!("Error");
-                }
-            };
-
-            dbg!(state);
-
-            thread::sleep(Duration::new(5, 0))
+            thread::sleep(Duration::new(15, 0))
         }
-    }).unwrap();
+    })
+    .unwrap();
 }
